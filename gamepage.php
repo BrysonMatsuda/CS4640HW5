74b8225644a5466a7040180378623ffcff3caf02
Bryson Matsuda <han3wf@virginia.edu>
2024-03-28 21:15:24 -0400

    started implementation of gamepage

--- gamepage.php
+++ gamepage.php
@@ -11,46 +11,43 @@
 
 <body>
     
-<div class="container" style="margin-top: 15px;">
-        <div class="row">
-                <div class="col-xs-12">
-                <h1>Connections Game</h1>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center mb-4">Connections Game</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>User: <?php echo $name; ?></p>
+            <p>Email: <?php echo $email; ?></p>
+            <p>Number of Guesses: <?php echo $guesses; ?></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header text-center">
+                    Words
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                <?=$message?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-
-                <div class="card">
-                    <div class="card-header">
-                        Words
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title"><?=$question["question"]?></h5>
-                    </div>
-                </div>
-                </div>
-                
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                <form action="?command=answer" method="post">
-                    <input type="hidden" name="questionid" value="<?=$question["id"]?>">
-
-                    <div class="mb-3">
-                        <label for="answer" class="form-label">Guess: </label>
-                        <input type="text" class="form-control" id="trivia-answer" name="answer">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Submit Guess</button>
-                </form>
+                <div class="card-body">
+                    <?php foreach ($selectedCategories as $category => $words): ?>
+                        <h5 class="card-title text-center"><?= $category ?></h5>
+                        <ul>
+                            <?php foreach ($words as $word): ?>
+                                <li><?= $word ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endforeach; ?>
+                    <p>Enter numeric IDs of your guess (space separated):</p>
+                    <form action="?command=answer" method="post">
+                        <input type="text" class="form-control mb-3" id="connections-answer" name="answer">
+                        <button type="submit" class="btn btn-primary">Submit Guess</button>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
